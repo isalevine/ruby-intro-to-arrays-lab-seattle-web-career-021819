a2f5e38e1ea1c3fbbba01ebe9bdfd705c2a7faff
Isa Levine <isaacdlevine@gmail.com>
2019-01-13 02:23:43 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  array = []
+end
+
+def array_with_two_elements
+  array = [1, 2]
+end
+
+def first_element(aaaarg)
+  aaaarg[0]
+end
+
+def third_element(aaaarg)
+  aaaarg[2]
+end
+
+def last_element(aaaarg)
+  aaaarg[-1]
+end
+
+def first_element_with_array_methods(blerg)
+  blerg.first
+end
+
+def last_element_with_array_methods(blerg)
+  blerg.last
+end
+
+def length_of_array(blerg)
+  blerg.count
+end

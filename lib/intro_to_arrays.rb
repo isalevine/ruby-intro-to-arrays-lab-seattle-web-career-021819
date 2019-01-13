d0d5af582f9645fc2a5986245f50d3bb2d584528
Isa Levine <isaacdlevine@gmail.com>
2019-01-13 02:21:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -3,7 +3,8 @@ def instantiate_new_array
 end
 
 def array_with_two_elements(element_one, element_two)
-  array = [element_one, element_two]
+  array << element_one
+  array << element_two
 end
 
 def first_element(aaaarg)
